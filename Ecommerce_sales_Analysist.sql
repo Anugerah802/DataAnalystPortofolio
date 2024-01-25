9a6033fc712ffc32d3121564caa52a776ea1f72d
Anugerah802 <154030102+Anugerah802@users.noreply.github.com>
2024-01-25 12:18:05 +0700

    Update Ecommerce_sales_Analysist.sql

--- Ecommerce_sales_Analysist.sql
+++ Ecommerce_sales_Analysist.sql
@@ -1,10 +1,11 @@
--- E-COMMERCE SALES ANALYSIST
--- Find Top Transaction in 2021
+-- ## E-COMMERCE SALES ANALYSIST ## --
+-- Find Top Transaction in 2021, by month
 
 SELECT  Year(order_date) as Year, Monthname(order_date) as Month, 
         sum(after_discount) as Total_transaction
 FROM order_detail
 where Year(order_date) = 2021
+and is_valid = 1
 Group by Monthname(order_date)
 order by sum(after_discount) desc;
 
@@ -15,18 +16,19 @@ SELECT Year(O.order_date) as Year, S.category, sum(O.after_discount) as Total_tr
 FROM order_detail as O
 Join sku_detail as S on (S.sku_Id = O.Id_sku)
 where Year(order_date) = 2022
+and O.is_valid = 1
 Group by S.category
 order by sum(O.after_discount) desc;
 
 
 -- By Category, compare transaction between 2021 and 2022, find which groups experienced increases and decreases
--- CTE
 
 WITH Category_transaction2021 as (
     SELECT Year(O.order_date) as Year, S.category, sum(O.after_discount) as Total_transaction2021  
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
     where Year(order_date) = 2021
+    and O.is_valid = 1
     Group by S.category
     ),
     Category_transaction2022 as (
@@ -34,6 +36,7 @@ WITH Category_transaction2021 as (
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
     where Year(order_date) = 2022
+    and O.is_valid = 1
     Group by S.category
     )
 SELECT  Category_transaction2021.Category, Total_transaction2021, Total_transaction2022,
@@ -45,54 +48,83 @@ Order by GrowPercentage desc;
 
 -- Base on order, find top 5 payment methode mostly used
 
-SELECT  O.payment_Id, P.payment_method, sum(O.qty_ordered) as Total_Order 
+SELECT  O.payment_Id, P.payment_method, count(O.qty_ordered) as Total_Order 
 FROM order_detail as O
 Join payment_detail as P on (P.payment_Id = O.payment_Id)
+WHERE O.is_valid = 1
 GROUP by P.payment_method
 ORDER by Total_Order desc
 Limit 5;
 
 
 -- sort by transaction value on this group of product (Samsung, Apple, Sony, Huawei, Lenovo)
--- CTE
+
 WITH Gadget_transaction as (
-    SELECT  S.sku_Id as Product_Id, S.sku_name as ProductName, O.price as Price, 
+    SELECT  S.sku_name as ProductName, O.price as Price, 
             O.qty_ordered as Total_order, O.after_discount as Total_transaction,
-            'Samsung' as product_group
+            'Samsung' as product_brand
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
     WHERE S.sku_name like '%Samsung%'
+    and O.is_valid = 1
     UNION
-    SELECT  S.sku_Id as Product_Id, S.sku_name as ProductName, O.price as Price, 
+    SELECT  S.sku_name as ProductName, O.price as Price, 
             O.qty_ordered as Total_order, O.after_discount as Total_transaction,
-            'Apple' as product_group
+            'Apple' as product_brand
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
-    WHERE S.sku_name like '%Apple%'
+    WHERE S.sku_name like '%Apple%' or S.sku_name like '%Iphone%'
+    and O.is_valid = 1
     UNION
-    SELECT  S.sku_Id as Product_Id, S.sku_name as ProductName, O.price as Price, 
+    SELECT  S.sku_name as ProductName, O.price as Price, 
             O.qty_ordered as Total_order, O.after_discount as Total_transaction,
-            'Sony' as product_group
+            'Sony' as product_brand
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
     WHERE S.sku_name like '%Sony%'
+    and O.is_valid = 1
     UNION
-    SELECT  S.sku_Id as Product_Id, S.sku_name as ProductName, O.price as Price, 
+    SELECT  S.sku_name as ProductName, O.price as Price, 
             O.qty_ordered as Total_order, O.after_discount as Total_transaction,
-            'Huawei' as product_group
+            'Huawei' as product_brand
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
     WHERE S.sku_name like '%Huawei%'
+    and O.is_valid = 1
     UNION
-    SELECT  S.sku_Id as Product_Id, S.sku_name as ProductName, O.price as Price, 
+    SELECT  S.sku_name as ProductName, O.price as Price, 
             O.qty_ordered as Total_order, O.after_discount as Total_transaction,
-            'Lenovo' as product_group
+            'Lenovo' as product_brand
     FROM order_detail as O
     Join sku_detail as S on (S.sku_Id = O.Id_sku)
-    WHERE S.sku_name like '%Lenovo%')
-SELECT  product_group, sum(Total_order) as Total_order, 
+    WHERE S.sku_name like '%Lenovo%'
+    and O.is_valid = 1)
+SELECT  product_brand, sum(Total_order) as Total_order, 
         sum(Total_transaction) as Total_transaction
 FROM Gadget_transaction
-GROUP by product_group
+GROUP by product_brand
 ORDER by Total_transaction desc;
 
+-- sort by transaction value on this group of product (Samsung, Apple, Sony, Huawei, Lenovo) with if condition
+
+WITH Gadget_transaction as (
+    SELECT  
+        CASE 
+            When S.sku_name like '%Samsung%' then 'Samsung'
+            When S.sku_name like '%Apple%' or S.sku_name like '%iphone%' then 'Apple'
+            When S.sku_name like '%Sony%' then 'Sony'
+            When S.sku_name like '%Huawei%' then 'Huawei'
+            When S.sku_name like '%Lenovo%' then 'Lenovo'
+        End as product_brand, 
+        O.qty_ordered as Total_order, 
+        O.after_discount as Total_transaction
+    FROM order_detail as O
+    Join sku_detail as S on (S.sku_Id = O.Id_sku)
+    WHERE O.is_valid = 1
+)
+SELECT  product_brand, sum(Total_order) as Total_order, 
+        sum(Total_transaction) as Total_transaction
+FROM Gadget_transaction
+WHERE product_brand is Not null
+GROUP by product_brand
+ORDER by Total_transaction Desc;
